40fc9687ee1ddd394953f7d1e368ab7b6f930990
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-04-29 02:07:57 +0600

    first commit

---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1586FD",
+    },
+    secondary: {
+      main: "#666F73",
+    },
+  },
+});
