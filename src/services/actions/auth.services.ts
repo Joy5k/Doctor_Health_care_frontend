7fb394852952fc2a883086b0d7275f2527dfff43
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-04 02:21:19 +0600

    added utils

--- src/services/actions/auth.services.ts
+++ src/services/actions/auth.services.ts
@@ -1,6 +1,28 @@
 import { authKey } from "@/app/constants/authKey"
-import { setToLocalStorage } from "@/utils/local-storage"
+import { getFromLocalStorage, setToLocalStorage } from "@/utils/local-storage"
 
 export const storeUserInfo = ({accessToken}:{accessToken:string}) => {
     return setToLocalStorage(authKey, accessToken)
 }
+
+export const getUserInfo = () => {
+    const authToken = getFromLocalStorage(authKey)
+    if (authToken) {
+        const decoded:any = decodedToken(authToken)
+        return{
+            ... decoded,
+            role:decoded.role.toLowerCase()
+        }
+    }
+    
+}
+export const isLoggedIn = () => {
+    const authToken = getFromLocalStorage(authKey);
+    if (authToken) {
+       return !!authToken;
+    }
+};
+
+export const removeUser = () => {
+    return removeFromLocalStorage(authKey);
+ };
