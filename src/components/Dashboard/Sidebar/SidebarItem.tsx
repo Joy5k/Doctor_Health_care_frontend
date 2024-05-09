fcac42f7b2831515ff79abe115857153e17e3b39
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-10 00:50:30 +0600

    created active button

--- src/components/Dashboard/Sidebar/SidebarItem.tsx
+++ src/components/Dashboard/Sidebar/SidebarItem.tsx
@@ -8,15 +8,22 @@ import Link from "next/link";
 import MailIcon from "@mui/icons-material/Mail";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import { DrawerItem } from "@/types";
+import { usePathname } from "next/navigation";
 
 type TProps = {
   item: DrawerItem;
 };
 
 const SidebarItem = ({ item }: TProps) => {
+    const linkPath = `/dashboard/${item.path}`
+    const pathname=usePathname()
   return (
-    <Link href="/">
-      <ListItem  disablePadding>
+    <Link href={linkPath }>
+          <ListItem disablePadding sx={{
+              ...(linkPath === pathname ? {
+              borderRight:"3x solid #1586FD" ,"& svg":{color:"#1586FD"}
+          }:{})
+      }}>
         <ListItemButton>
           <ListItemIcon>{item.icon && <item.icon></item.icon>}</ListItemIcon>
           <ListItemText primary={item.title} />
