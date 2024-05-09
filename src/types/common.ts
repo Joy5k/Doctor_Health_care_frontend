c8ab95680af6fbea96f9aab15d1171e5f102ba1b
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-10 00:26:07 +0600

    created the sidebar manu items

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,8 +1,19 @@
 import { USER_ROLE } from "@/app/constants/role";
+import { SvgIconTypeMap } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 
 export type IMeta = {
     page: number;
     limit:number;
     total: number;
 }
-export type UserRole=keyof typeof USER_ROLE
+export type UserRole = keyof typeof USER_ROLE
+
+export interface DrawerItem {
+    title: string;
+    path: string;
+    parentPath?: string;
+    icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> & { muiName: string };
+    child?: DrawerItem[];
+  }
+  
