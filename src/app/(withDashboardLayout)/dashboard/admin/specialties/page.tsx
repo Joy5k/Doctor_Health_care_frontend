dd17a19818a3abc5d2c464ab4d6f16f143fba99b
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-13 01:23:30 +0600

    created the redux data get query and post mutation

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,18 +1,20 @@
-import PHModal from '@/components/Shared/PHModal/PHModal';
-import { Box, Button, Stack, TextField } from '@mui/material';
-import React, { useState } from 'react';
+import PHModal from "@/components/Shared/PHModal/PHModal";
+import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
+import { Box, Button, Stack, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const SpecialtiesPage = () => {
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const {data,isLoading} = useGetAllSpecialtiesQuery({});
 
-    return (
-        <Box> 
-            <Stack direction='row' justifyContent="space-between" alignItems='center'>
-                <Button>Search Specialty</Button>
-                <TextField size='small' placeholder='Search specialties'></TextField>
-            </Stack>
-        </Box>
-    );
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button>Search Specialty</Button>
+        <TextField size="small" placeholder="Search specialties"></TextField>
+      </Stack>
+    </Box>
+  );
 };
 
 export default SpecialtiesPage;
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { axiosBaseQuery } from '@/helpers/axios/axiosBaseQuery'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { tagTypesList } from '../tag-types'
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
@@ -7,4 +8,5 @@ export const baseApi = createApi({
   baseQuery: axiosBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
   endpoints: (builder) => ({
   }),
+  tagTypes:tagTypesList
 })
