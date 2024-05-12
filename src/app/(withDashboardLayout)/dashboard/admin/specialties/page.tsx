65886923dde18252ba339268461531659765e831
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-13 02:02:48 +0600

    created the specialties delete mutation

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,18 +1,67 @@
-import PHModal from "@/components/Shared/PHModal/PHModal";
-import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
-import { Box, Button, Stack, TextField } from "@mui/material";
+import { useDeleteSpecialtyMutation, useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
+import { Box, Button, IconButton, Stack, TextField } from "@mui/material";
 import React, { useState } from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Image from "next/image";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { toast } from "sonner";
 
 const SpecialtiesPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const {data,isLoading} = useGetAllSpecialtiesQuery({});
-
+    const { data, isLoading } = useGetAllSpecialtiesQuery({});
+    const [deleteSpecialties]=useDeleteSpecialtyMutation()
+    const handleDelete = async(id: string) => {
+        console.log(id);
+        try {
+            const res = await deleteSpecialties(id).unwrap();
+            if (res?.id) {
+                toast.success("Specialties deleted successfully")
+            }
+        } catch (error:any) {
+            console.log(error.message);
+        }
+    }
+  const columns: GridColDef[] = [
+    { field: "title", headerName: "Title", width: 400 },
+    {
+      field: "icon",
+      headerName: "Icon",
+     flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Image src={row.icon} width={30} height={30} alt="icon"></Image>
+          </Box>
+        );
+      },
+    },
+    {
+      field: "Action",
+      headerName: "Action",
+        flex: 1,
+        headerAlign: "center",
+        align: "center",
+        renderCell: ({ row }) => {
+        return (
+          <IconButton onClick={()=>handleDelete(row.id)} aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        );
+      },
+    },
+  ];
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Button>Search Specialty</Button>
         <TextField size="small" placeholder="Search specialties"></TextField>
       </Stack>
+      {!isLoading ? (
+        <Box my={2}>
+          <DataGrid rows={data} columns={columns} />
+        </Box>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </Box>
   );
 };
