2b319d7e364bbe3bd531ee4c444813b87f57d3ba
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-14 02:36:26 +0600

    created the schedules created route

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/page.tsx
@@ -1,9 +1,30 @@
+import { Box, Button, Stack, TextField } from "@mui/material";
+import DoctorModal from "../doctors/components/DoctorModal";
+import { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
 
 const SchedulesPage = () => {
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
     return (
-        <div>
-            <h2>This is schedule page</h2>
-        </div>
+        <Box>
+        <Stack direction="row" justifyContent="space-between" alignItems="center">
+          <Button onClick={() => setIsModalOpen(true)}>Create New Doctor</Button>
+          <DoctorModal open={isModalOpen} setOpen={setIsModalOpen}></DoctorModal>
+          <TextField
+            // onChange={(e) => setSearchTerm(e.target.value)}
+            size="small"
+            placeholder="Search doctor"
+          ></TextField>
+        </Stack>
+        {/* {!isLoading ? (
+          <Box my={2}>
+            <DataGrid rows={doctors} columns={columns} />
+          </Box>
+        ) : (
+          <h1>Loading...</h1>
+        )} */}
+      </Box>
     );
 };
 
