4472dc392aa3e952d528b22ebc609558fa392849
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-14 03:37:09 +0600

    created the date formatter

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
@@ -1,6 +1,7 @@
 import PHDatePicker from "@/components/Forms/PHdatePicker";
 import PHForms from "@/components/Forms/PHForms";
 import PHModal from "@/components/Shared/PHModal/PHModal";
+import { dateFormatter } from "@/utils/dateFormatter";
 import { Button, Grid } from "@mui/material";
 import { useState } from "react";
 import { FieldValues } from "react-hook-form";
@@ -12,8 +13,11 @@ type TProps = {
 
 const ScheduleModal = ({ open, setOpen }: TProps) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const handleFormSubmit = async (values: FieldValues) => {
-    try {
+    const handleFormSubmit = async (values: FieldValues) => {
+       values.startDate=dateFormatter(values.startDate)
+       values.endDate=dateFormatter(values.endDate)
+      try {
+        
     } catch (error: any) {
       console.error(error.message);
     }
@@ -32,8 +36,14 @@ const ScheduleModal = ({ open, setOpen }: TProps) => {
           <Grid item spacing={12}>
             <PHDatePicker name="endDate" label="End Date"></PHDatePicker>
           </Grid>
+          <Grid item spacing={6}>
+            <PHDatePicker name="startTime" label="Start Time"></PHDatePicker>
+          </Grid>
+          <Grid item spacing={6}>
+            <PHDatePicker name="endTIme" label="End Time"></PHDatePicker>
+          </Grid>
         </Grid>
-        <Button type="submit">Create </Button>
+        <Button type="submit" sx={{mt:1}}>Create </Button>
       </PHForms>
     </PHModal>
   );
