d824d97a8c4695d84c5c58cdbbbdab0aeaf2e20f
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-14 03:46:12 +0600

    created the schedule creation method

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/components/ScheduleModal.tsx
@@ -1,10 +1,13 @@
 import PHDatePicker from "@/components/Forms/PHdatePicker";
 import PHForms from "@/components/Forms/PHForms";
 import PHModal from "@/components/Shared/PHModal/PHModal";
+import { useCreateScheduleMutation } from "@/redux/api/scheduleApi";
 import { dateFormatter } from "@/utils/dateFormatter";
+import { timeFormatter } from "@/utils/timeFormatter";
 import { Button, Grid } from "@mui/material";
 import { useState } from "react";
 import { FieldValues } from "react-hook-form";
+import { toast } from "sonner";
 
 type TProps = {
   open: boolean;
@@ -13,11 +16,20 @@ type TProps = {
 
 const ScheduleModal = ({ open, setOpen }: TProps) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-    const handleFormSubmit = async (values: FieldValues) => {
-       values.startDate=dateFormatter(values.startDate)
-       values.endDate=dateFormatter(values.endDate)
-      try {
-        
+  const [createSchedule] = useCreateScheduleMutation();
+
+  const handleFormSubmit = async (values: FieldValues) => {
+    values.startDate = dateFormatter(values.startDate);
+    values.endDate = dateFormatter(values.endDate);
+    values.startTime = timeFormatter(values.startTime);
+    values.endTime = timeFormatter(values.endTime);
+    try {
+      const res = await createSchedule(values).unwrap();
+      // console.log(res);
+      if (res?.length) {
+        toast.success("Schedules created successfully!");
+        setOpen(false);
+      }
     } catch (error: any) {
       console.error(error.message);
     }
@@ -29,7 +41,7 @@ const ScheduleModal = ({ open, setOpen }: TProps) => {
       title="Create Schedule"
     >
       <PHForms onSubmit={handleFormSubmit}>
-        <Grid container spacing={2} sx={{width:"400px"}}>
+        <Grid container spacing={2} sx={{ width: "400px" }}>
           <Grid item spacing={12}>
             <PHDatePicker name="startDate" label="Start Date"></PHDatePicker>
           </Grid>
@@ -43,7 +55,9 @@ const ScheduleModal = ({ open, setOpen }: TProps) => {
             <PHDatePicker name="endTIme" label="End Time"></PHDatePicker>
           </Grid>
         </Grid>
-        <Button type="submit" sx={{mt:1}}>Create </Button>
+        <Button type="submit" sx={{ mt: 1 }}>
+          Create{" "}
+        </Button>
       </PHForms>
     </PHModal>
   );
