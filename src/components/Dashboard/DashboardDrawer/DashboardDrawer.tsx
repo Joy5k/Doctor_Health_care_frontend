9fb5685f9d168009e616af8ac168e591d3ab54cf
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-15 17:54:54 +0600

    created the dashboard user profile section

--- src/components/Dashboard/DashboardDrawer/DashboardDrawer.tsx
+++ src/components/Dashboard/DashboardDrawer/DashboardDrawer.tsx
@@ -1,20 +1,27 @@
-"use client"
+"use client";
 
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Sidebar from '../Sidebar/Sidebar';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Avatar, Badge, Stack } from "@mui/material";
+import AccountMenu from "../AccountMenu/AccountMenu";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import { useGetSingleUserQuery } from "@/redux/api/userApi";
+import Sidebar from "../Sidebar/Sidebar";
 
 const drawerWidth = 240;
 
-
-export default function DashboardDrawer({ children }:{children:React.ReactNode}) {
+export default function DashboardDrawer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
 
@@ -33,19 +40,21 @@ export default function DashboardDrawer({ children }:{children:React.ReactNode})
     }
   };
 
-
+  const { data, isLoading } = useGetSingleUserQuery({});
+  // console.log(data);
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
         sx={{
           width: { sm: `calc(100% - ${drawerWidth}px)` },
           ml: { sm: `${drawerWidth}px` },
-          background: "#f4f7fe",
-          borderBottom: 0,
-          border:"1px solid lightgray"
+          background: "#F4F7FE",
+          boxShadow: 0,
+          borderBottom: "1px solid #ddd",
+          py: 1,
         }}
       >
         <Toolbar>
@@ -54,18 +63,46 @@ export default function DashboardDrawer({ children }:{children:React.ReactNode})
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' },color:"primary.main" }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
-            <MenuIcon />
+            <MenuIcon sx={{ color: "primary.main" }} />
           </IconButton>
-          <Box>
-          <Typography variant="body2" noWrap component="div" color="gray">
-            Responsive drawer
-          </Typography>
-          <Typography variant="body2" noWrap component="div" color="gray">
-            Welcome To, PH Health care
-          </Typography>
-         </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <Box>
+              <Typography
+                variant="body2"
+                noWrap
+                component="div"
+                sx={{ color: "rgba(11, 17, 52, 0.6)" }}
+              >
+                Hi, {isLoading ? "Loading..." : data?.name},
+              </Typography>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ color: "primary.main" }}
+              >
+                Welcome to PH Health Care!
+              </Typography>
+            </Box>
+            <Stack direction="row" gap={3}>
+              <Badge badgeContent={1} color="primary">
+                <IconButton sx={{ background: "#ffffff" }}>
+                  <NotificationsNoneIcon color="action" />
+                </IconButton>
+              </Badge>
+              <Avatar alt={data?.name} src={data?.profilePhoto} />
+              <AccountMenu />
+            </Stack>
+          </Box>
         </Toolbar>
       </AppBar>
       <Box
@@ -75,7 +112,6 @@ export default function DashboardDrawer({ children }:{children:React.ReactNode})
       >
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
-         
           variant="temporary"
           open={mobileOpen}
           onTransitionEnd={handleDrawerTransitionEnd}
@@ -84,31 +120,39 @@ export default function DashboardDrawer({ children }:{children:React.ReactNode})
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
-         <Sidebar/>
+          <Sidebar />
         </Drawer>
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
-          <Sidebar/>
+          <Sidebar />
         </Drawer>
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
         <Toolbar />
-              <Box>
-                  {children}
-       </Box>
+        <Box>{children}</Box>
       </Box>
     </Box>
   );
