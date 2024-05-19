b3afa67c2a07b69cb7de521d6e151b4b3d0d07d9
Mehedi Hasan Joy <mmehedihasanjoyv@gmail.com>
2024-05-20 03:36:43 +0600

    created the middleware and reusable cookies handler

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,8 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { jwtDecode } from 'jwt-decode';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
 type Role = keyof typeof roleBasedPrivateRoutes;
 
 const AuthRoutes = ['/login', '/register'];
@@ -16,12 +19,50 @@ const roleBasedPrivateRoutes = {
 };
 
 export function middleware(request: NextRequest) {
-    const { pathname } = request.nextUrl
-        
+   const { pathname } = request.nextUrl;
+   console.log({ pathname });
+   const accessToken = cookies().get('accessToken')?.value;
 
-  return NextResponse.redirect(new URL('/', request.url))
+   if (!accessToken) {
+      if (AuthRoutes.includes(pathname)) {
+         return NextResponse.next();
+      } else {
+         return NextResponse.redirect(new URL('/login', request.url));
+      }
+   }
+
+   console.log({ pathname });
+
+   if (
+      accessToken &&
+      (commonPrivateRoutes.includes(pathname) ||
+         commonPrivateRoutes.some((route) => pathname.startsWith(route)))
+   ) {
+      return NextResponse.next();
+   }
+
+   let decodedData = null;
+
+   if (accessToken) {
+      decodedData = jwtDecode(accessToken) as any;
+   }
+
+   const role = decodedData?.role;
+
+   // if (role === 'ADMIN' && pathname.startsWith('/dashboard/admin')) {
+   //    return NextResponse.next();
+   // }
+
+   if (role && roleBasedPrivateRoutes[role as Role]) {
+      const routes = roleBasedPrivateRoutes[role as Role];
+      if (routes.some((route) => pathname.match(route))) {
+         return NextResponse.next();
+      }
+   }
+
+   return NextResponse.redirect(new URL('/', request.url));
 }
 
 export const config = {
-  matcher: '/dashboard/:path*',
-}
+   matcher: ['/login', '/register', '/dashboard/:page*', '/doctors/:page*'],
+};
